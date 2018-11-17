ed4ea3b0e6ddf8eb213d7273fb1617c7301aaf42
Roman <norromanv@yandex.ru>
2018-11-18 00:35:18 +0300

    pyramid task done

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -13,63 +13,58 @@ public class PyramidBuilder {
      * @return 2d array with pyramid inside
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
-    public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+
+    private int floors = 0;
+    private boolean canBuild = false;
+    private int numberOfBlocks = 0;
+    private int step = 1;
+    private int width;
+    private int height;
+
+    public int[][] buildPyramid(List<Integer> inputNumbers) throws CannotBuildPyramidException {
+
+        if(inputNumbers.size()>100){
+            throw new CannotBuildPyramidException();
+        } else {
+            if (inputNumbers.contains(null)) {
+                throw new CannotBuildPyramidException();
+            }
+            else {
+                Collections.sort(inputNumbers);
+            }
+        }
+
+        for (int x : inputNumbers) {
+            numberOfBlocks = numberOfBlocks + step;
+            floors++;
+            if (numberOfBlocks == inputNumbers.size()) {
+                canBuild = true;
+                break;
+            }
+            step++;
+        }
+
+        int[][] intArray;
+        if (canBuild == true) {
+            height = floors;
+            width = floors * 2 - 1;
+            int k = inputNumbers.size() - 1;
+
+            intArray = new int[height][width];
+            for (int y = height - 1; y >= 0; y--) {
+                int counter = (height - 1) - y;
+                int numberOfZeros = counter;
+                for (int z = width - 1; z >= 0; z--) {
+                    if ((z == width - 1 - counter) && (z >= numberOfZeros)) {
+                        intArray[y][z] = inputNumbers.get(k);
+                        k--;
+                        counter = counter + 2;
+                    }
+                }
+            }
+        } else {
+            throw new CannotBuildPyramidException();
+        }
+        return intArray;
     }
-
-//    int floors = 0; //number of floors of pyramide
-//    boolean canBuild = false;
-//    int numberOfBlocks = 0;//number of pyramide blocks (numbers)
-//    int step = 1;
-//    int width;
-//    int height;
-//
-//    public int[][] buildPyramid(List<Integer> inputNumbers) throws CannotBuildPyramidException {
-//        if(inputNumbers.size()>100){
-//            throw new CannotBuildPyramidException("too big");
-//        }
-//        else {
-//            if (inputNumbers.contains(null)) {
-//                throw new CannotBuildPyramidException("null element");
-//            } else {
-//                Collections.sort(inputNumbers);
-//            }
-//        }
-//        for (int x : inputNumbers) {
-//            numberOfBlocks = numberOfBlocks + step;
-//            floors++;
-//            if (numberOfBlocks == inputNumbers.size()) {
-//                canBuild = true;
-//                break;
-//            }
-//            step++;
-//        }
-//
-//        int[][] intArray;
-//        if (canBuild == true) {
-//            height = floors;
-//            width = floors * 2 - 1;
-//            int k = inputNumbers.size() - 1;
-//
-//
-//            intArray = new int[height][width];
-//            for (int y = height - 1; y >= 0; y--) {
-//                int counter = (height - 1) - y;
-//                int numberOfZeros = counter;
-//                for (int z = width - 1; z >= 0; z--) {
-//                    if ((z == width - 1 - counter) && (z >= numberOfZeros)) {
-//                        intArray[y][z] = inputNumbers.get(k);
-//                        k--;
-//                        counter = counter + 2;
-//                    }
-//                }
-//            }
-//        } else {
-//            throw new CannotBuildPyramidException("cannot build pyramid");
-//        }
-//        return intArray;
-//    }
-
-
 }
--- src/test/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilderTest.java
+++ src/test/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilderTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class PyramidBuilderTest {
+public class  PyramidBuilderTest {
 
     private PyramidBuilder pyramidBuilder = new PyramidBuilder();
 
